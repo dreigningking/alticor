02028c9ede1ba1de425324caf3f60f4ef5e406a3
dreigningking <reigningkingforever@gmail.com>
2023-02-07 04:44:36 +0100

    what is goingo on oo

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('index');
+Route::get('/', function(){return view('index');})->name('index');
 Route::group(['prefix'=> 'about','as'=>'about.'],function(){
     Route::view('vision-and-core-values', 'about.vision_core_values')->name('vision-and-core-values');
     Route::view('ourway', 'index')->name('ourway');
