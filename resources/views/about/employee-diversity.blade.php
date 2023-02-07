874aa808ebf78b7d7bdfa280bcc666c6c5e5fa77
dreigningking <reigningkingforever@gmail.com>
2023-02-07 12:50:43 +0100

    added diversity

---
+++ resources/views/about/employee-diversity.blade.php
@@ -0,0 +1,310 @@
+@extends('1layout.others')
+@section('content')
+
+
+<div class="page-banner"
+    style="background-image:url({{asset('wp-content/uploads/2017/07/subcontractor-outreach-banner.jpg')}});">
+    <div class="page-banner-block">
+        <div class="page-banner-content">
+            <h1>Employee Diversity and Inclusion</h1>
+        </div>
+    </div>
+</div>
+
+
+<div class="breadcumb" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <div class="wrapper clearfix">
+    </div>
+</div>
+
+<div class="main-container">
+    <div class="wrapper clearfix">
+
+
+
+
+
+
+        <div class="main-content">
+
+            <h2 id="h-creating-an-inclusive-environment">Creating an Inclusive Environment</h2>
+
+
+
+            <div class="wp-container-2 wp-block-columns">
+                <div class="wp-container-1 wp-block-column">
+                    <p><img class="alignright"
+                            src="{{asset('wp-content/uploads/2022/06/United-Rentals-award-300x248.jpg')}}"
+                            alt="" width="255" height="211" />Cultivating a diverse workforce is an
+                        important part of Alticor Inc’ culture. Promoting a diverse workplace elevates human value and
+                        productivity by respecting and utilizing the viewpoints of workers from different backgrounds.
+                        We are a committed Equal Opportunity Employer and encourage open dialogue and understanding
+                        among our employees. Our workforce diversity allows Alticor Inc to better represent the
+                        communities in which we live and work and to ensure that our employees are the best and
+                        brightest in the industry.</p>
+                    <p>Alticor Inc recognizes that employees need to feel like they belong within an organization.
+                        Resource groups, on-going trainings and teambuilding activities create a community environment
+                        that welcomes individuals and embraces their differences. Most recently, Alticor Inc created an
+                        internal forum dedicated to this important cause, DEIc.</p>
+                </div>
+            </div>
+
+
+
+            <figure
+                class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                <div class="wp-block-embed__wrapper">
+                    <iframe loading="lazy" title="Building Together a Culture of Diversity and Inclusion" width="640"
+                        height="360" src="https://www.youtube.com/embed/TmuuQuiggfk?feature=oembed" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+            </figure>
+
+
+
+            <hr class="wp-block-separator has-alpha-channel-opacity" />
+
+
+
+            <h2>Diversity, Equity, Inclusion and community (DEIc) Forums</h2>
+
+
+
+            <div class="wp-container-5 wp-block-columns">
+                <div class="wp-container-3 wp-block-column">
+                    <p>In support of its recently launched core values, Alticor Inc launched the Diversity, Equity,
+                        Inclusion and community (DEIc) forums, consisting of a core group of volunteers from each
+                        district. These forums have been working hard to foster healthy discussions in their districts
+                        and develop ideas and trainings to fortify our commitment to diversity and community. These
+                        groups support employees in each district, working to maintain internal morale and community
+                        ties through outreach, mentoring, recruiting and advancement-related efforts.</p>
+
+
+
+                    <p>Associated Builders and Contractors (ABC) has awarded Alticor Inc the National Diversity
+                        Excellence Award in the General Contractor category nine times since the award’s inception in
+                        2012. This national award is judged on the diversity of the company’s own workforce hiring
+                        practices, training, and retention, as well as our relationships with small disadvantaged,
+                        minority, or women-owned subcontractors and vendors.</p>
+                </div>
+
+
+
+                <div class="wp-container-4 wp-block-column is-vertically-aligned-top">
+                    <figure
+                        class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                        <div class="wp-block-embed__wrapper">
+                            <iframe loading="lazy" title="Alticor Inc - Diversity, Equity, Inclusion and Community"
+                                width="640" height="360"
+                                src="https://www.youtube.com/embed/jg-zzGE5-XE?feature=oembed" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+
+
+
+            <blockquote class="wp-block-quote">
+                <p><em>“I take our core value of diversity very seriously. I understand the advantages that a diverse
+                        workforce can bring to our business and recognize that diversity goes hand in hand with
+                        inclusion and equity. That collectively, this mixture of differences in characteristics,
+                        experiences and backgrounds combined with the similarities of our Alticor Inc core values, makes
+                        us a stronger company.”</em></p>
+                <p><em>– Mike Choutka, Alticor Inc President &amp; CEO</em></p>
+            </blockquote>
+
+
+
+            <p>Here are a few ways Alticor Inc is supporting diversity, both within the company and throughout the
+                construction industry:</p>
+
+
+
+            <div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+
+            <div class="wp-container-8 wp-block-columns">
+                <div class="wp-container-6 wp-block-column has-white-background-color has-background">
+                    <h4><strong>Promoting Women in Construction</strong></h4>
+
+
+
+                    <p>With twice the national average of women in construction, Alticor Inc supports women in the AEC
+                        industry. From educating aspiring young high-schoolers through programs like <a
+                            href="https://www.henselphelps.com/hensel-phelps-supports-ace-mentor-program/">ACE</a>, to
+                        recruiting top talent at hundreds of college campuses nationwide, Alticor Inc encourages the
+                        mentoring, hiring and growth of women in construction. In addition to being an industry leader,
+                        internally, Alticor Inc supports its employees through programs such as the Women’s Leadership
+                        Seminar (WLS), an event designed to bring together women from across the company to learn from
+                        one another, share challenges, and explore areas of personal and career growth, as well as WNet,
+                        a leadership network of women within the company.</p>
+                </div>
+
+
+
+                <div class="wp-container-7 wp-block-column is-vertically-aligned-top">
+                    <figure
+                        class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                        <div class="wp-block-embed__wrapper">
+                            <iframe loading="lazy" title="Building Together – Women in Craft" width="640"
+                                height="360" src="https://www.youtube.com/embed/g3BHlY22vw0?feature=oembed"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+
+
+
+            <div class="wp-container-11 wp-block-columns column-rev-order">
+                <div class="wp-container-9 wp-block-column is-vertically-aligned-top">
+                    <figure
+                        class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                        <div class="wp-block-embed__wrapper">
+                            <iframe loading="lazy" title="Building Together – Andre Cooper" width="640"
+                                height="360" src="https://www.youtube.com/embed/hRyXi0AU0-U?feature=oembed"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+                        </div>
+                    </figure>
+                </div>
+
+
+
+                <div class="wp-container-10 wp-block-column">
+                    <h4><strong>Supporting Underrepresented Minorities</strong></h4>
+
+
+
+                    <p>By engaging diverse businesses, providing recruiting opportunities and supporting national
+                        organizations, Alticor Inc is building a culture without boundaries where everyone can feel
+                        empowered and has equal opportunity. Alticor Inc has a long-standing relationship supporting
+                        Historically Black Colleges and Universities and organizations such as the Society of Hispanic
+                        Professional Engineers (SHPE), and the National Society of Black Engineers (NSBE). Internally,
+                        Alticor Inc supports their employees through educational resources, on-going training and
+                        encouraging employee resource groups such as BNet (Black Network), an organized group of Alticor
+                        Inc employees and executives coming together to support one another.</p>
+                </div>
+            </div>
+
+
+
+            <div class="wp-container-14 wp-block-columns">
+                <div class="wp-container-12 wp-block-column">
+                    <h4><strong>Advancing Veterans and Service Members</strong></h4>
+
+
+
+                    <p>Even when they are no longer in the military, veterans are <em>Always in Uniform</em>. They bring
+                        teamwork, integrity and respect into every aspect of their lives, including on the jobsite. They
+                        truly embody Alticor Inc’ Core Values&#8211;one of the many reasons why Alticor Inc encourages
+                        the hiring and support of military veterans. Alticor Inc is actively involved in organizations
+                        such as <a href="https://www.hiringourheroes.org/">Hiring Our Heroes</a>, <a
+                            href="https://skillbridge.osd.mil/">DOD SkillBridge</a> and <a
+                            href="https://recruitmilitary.com/">RecruitMilitary</a> and encourages all service members
+                        to apply.</p>
+
+
+
+                    <p>Alticor Inc is proud to support the ongoing commitments of our National Guard and Reserve
+                        employees. Alticor Inc supports these employees during deployments and is proud to welcome them
+                        home and work with them to transition back to the jobsite when their deployment ends. ​​​​​​​
+                    </p>
+                </div>
+
+
+
+                <div class="wp-container-13 wp-block-column is-vertically-aligned-top">
+                    <figure
+                        class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                        <div class="wp-block-embed__wrapper">
+                            <iframe loading="lazy" title="To Our Veterans - Thank You for Building America"
+                                width="640" height="360"
+                                src="https://www.youtube.com/embed/eSYVrE9eJt8?feature=oembed" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+                        </div>
+                    </figure>
+                </div>
+            </div>
+
+
+
+            <div class="wp-container-17 wp-block-columns column-rev-order">
+                <div class="wp-container-15 wp-block-column is-vertically-aligned-top">
+                    <figure
+                        class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+                        <div class="wp-block-embed__wrapper">
+                            <iframe loading="lazy" title="Building Together – Launi&#039;s Story" width="640"
+                                height="360" src="https://www.youtube.com/embed/ugsXxUXxa7I?feature=oembed"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen></iframe>
+                        </div>
+                    </figure>
+                </div>
+
+
+
+                <div class="wp-container-16 wp-block-column">
+                    <h4><strong>Building-Up Those Who Need It Most</strong></h4>
+
+
+
+                    <p>As a large contractor building in communities across the nation, Alticor Inc values the
+                        opportunity to contribute to the training and job possibilities for the local people, no matter
+                        what their past or circumstance may be. Alticor Inc believes in second chances and that everyone
+                        deserves an opportunity to build. Because of these beliefs, Alticor Inc continually partners
+                        with local re-entry and rehabilitation programs, with the goal of creating opportunities for
+                        individuals recently exiting the justice system.</p>
+
+
+
+                    <p>In addition to partnership opportunities, Alticor Inc has created its own program, the <a
+                            href="../hensel-phelps-and-path-continue-partnership-through-the-employment-opportunity-program/index.html">Employment
+                            Opportunity Program</a>, designed to connect the homeless community with job prospects in
+                        the construction industry. The Employment Opportunity Program allows individuals to build steady
+                        construction careers as well as improve their livelihoods for themselves and their families.</p>
+                </div>
+            </div>
+
+
+
+            <hr class="wp-block-separator has-alpha-channel-opacity" />
+
+
+
+            <h2 id="h-building-together">Building Together</h2>
+
+
+            <div class="wp-block-image">
+                <figure class="alignright size-large is-resized"><img loading="lazy"
+                        src="{{ asset('wp-content/uploads/2022/01/MicrosoftTeams-image-36-1024x576.jpg') }}"
+                        alt="" class="wp-image-12745" width="412" height="187" /></figure>
+            </div>
+
+
+            <p>Created by employees and partners, Building Together is Alticor Inc’ national diversity and inclusion
+                campaign. Building Together highlights the importance of diversity within Alticor Inc and the AEC
+                industry. The campaign gives employees the opportunity to share their stories and learn from each
+                other’s experiences.</p>
+
+
+
+            <p>Learn More About Alticor Inc <a
+                    href="https://www.henselphelps.com/employee-diversity/building-together/"><u>Building Together
+                        Campaign</u></a></p>
+
+        </div>
+    </div>
+
+
+</div>
+@endsection
