a7f2020411e808dc871daf9fb13b6d889c846647
ANON LEE <narutoiris@aol.com>
2023-02-14 05:10:01 -0500

    i'm doe

---
+++ resources/views/category/career/index.blade.php
@@ -0,0 +1,122 @@
+@extends('1layout.others')
+@section('content')
+
+
+
+
+<div class="page-banner" style="background-image:url({{asset('wp-content/themes/henselphelps/img/news-hero.jpg')}});">
+    <div class="page-banner-block">
+        <div class="page-banner-content">
+            <h1>News Center</h1>
+        </div>
+    </div>
+</div>
+<div class="wrapper">
+    <div id="primary" class="content-area">
+			<div class="page-content">
+            			<br/><h1 style="text-align:center;">Career</h1><br/><br/>
+            <div class="news-post-block clearfix">
+			<div>
+						<div class="post-box">
+                        <a href="../../celebrating-a-successful-career-kirk-j-hazen/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2023/01/Kirk-Celebration-Post1-1024x684.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2023/01/Kirk-Celebration-Post1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Celebrating a Successful Career: Kirk J....</h2>
+                                        <div class="post-excerpt">After more than three decades with Alticor Inc, Kirk Hazen, Vice President and District Manager of... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../orlando-business-journal-names-timothy-queely-a-2022-40-under-40-honoree/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/05/1-scaled-480x320.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/05/1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Orlando Business Journal Names Timothy Q...</h2>
+                                        <div class="post-excerpt">Orlando Business Journal (OBJ)&nbsp;named the Southeast District&#8217;s Timothy Queely one of&nbsp;2022&#8217;s 40 Under 40. Recognized for... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../hensel-phelps-veterans-network-vnet-works-to-support-hire-and-connect-the-veteran-community/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/05/NorCal-VNET-Lunch-1-scaled-480x320.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/05/NorCal-VNET-Lunch-1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Alticor Inc Veterans Network (VNET) Wo...</h2>
+                                        <div class="post-excerpt">Alticor Inc has a long history of supporting our military. This support led to the creation... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../a-lifetime-dedicated-to-service-always-in-uniform/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/05/Picture1-480x320.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/05/Picture1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">A Lifetime Dedicated to Service – Always...</h2>
+                                        <div class="post-excerpt">To show appreciation and recognition to all our Alticor Inc Veterans, National Guard and Reserve Members,... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../women-in-construction-insight-from-hensel-phelps-rising-leaders/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/03/BRAND-WIC-AD-Layouts-05-1-480x320.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/03/BRAND-WIC-AD-Layouts-05-1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="Women in Construction 2022" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Women In Construction – Insight from Hen...</h2>
+                                        <div class="post-excerpt">Djenita Svinjar Title: Corporate CounselYears with Alticor Inc: 3 Years Why is Diversity, Inclusion &amp; Equity... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../building-together-joel-kelley-iii/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/01/MicrosoftTeams-image-42-480x320.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/01/MicrosoftTeams-image-42-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Career                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Building Together – Joel Kelley III</h2>
+                                        <div class="post-excerpt">Alticor Inc Q&amp;A with Joel Kelley III: Name: Joel Kelley IIITitle: Project SuperintendentDistrict: SouthwestYears with Hensel... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+			        </div>
+        </div>
+		</div>
+		</div><!-- #main -->
+	</div><!-- #primary -->
+   
+    @endsection
---
+++ resources/views/category/community/index.blade.php
@@ -0,0 +1,120 @@
+@extends('1layout.others')
+@section('content')
+
+
+<div class="page-banner" style="background-image:url({{asset('wp-content/themes/henselphelps/img/news-hero.jpg')}});">
+    <div class="page-banner-block">
+        <div class="page-banner-content">
+            <h1>News Center</h1>
+        </div>
+    </div>
+</div>
+<div class="wrapper">
+    <div id="primary" class="content-area">
+			<div class="page-content">
+            			<br/><h1 style="text-align:center;">Community</h1><br/><br/>
+            <div class="news-post-block clearfix">
+			<div>
+						<div class="post-box">
+                        <a href="../../construction-executive-from-the-ground-up/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/12/QAE_Final_04-1024x684.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/12/QAE_Final_04-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Construction Executive from the Ground Up</h2>
+                                        <div class="post-excerpt">American Abatement &amp; Demo (AAD) CEO and President Mari Borrero has worked hard to build AAD... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../hensel-phelps-celebrates-a-culture-of-giving-back/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/12/BT-December-Website-Graphics_1200x720-1024x684.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/12/BT-December-Website-Graphics_1200x720-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Alticor Inc Celebrates a Culture of Gi...</h2>
+                                        <div class="post-excerpt">At Alticor Inc, we consider ourselves truly fortunate to pay it forward to the communities in... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../pacific-northwest-hosts-4th-annual-veterans-day-event/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/11/DSC_0554-1-768x512.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/11/DSC_0554-1-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="Alticor Inc and Permission to Start Dreaming at Westland Distillery" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Pacific Northwest Hosts 4th Annual Veter...</h2>
+                                        <div class="post-excerpt">Each year, the Alticor Inc Pacific Northwest Region hosts an annual Veterans Day Event to raise... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../hensel-phelps-recognizes-and-supports-awareness-for-mens-health/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/10/Article-1024x684.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/10/Article-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Alticor Inc Recognizes and Supports Aw...</h2>
+                                        <div class="post-excerpt">In November, Alticor Inc’ Building Together campaign will focus on Men’s Health by raising awareness and... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../hensel-phelps-is-proud-to-support-construction-inclusion-week/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/10/Construction-Inclusion-Web-Banner_1200x628.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/10/Construction-Inclusion-Web-Banner_1200x628-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Alticor Inc is Proud to Support Constr...</h2>
+                                        <div class="post-excerpt">An advocate for its mission, Alticor Inc is proud to participate once again in Construction Inclusion... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+						<div class="post-box">
+                        <a href="../../invested-in-womens-health-hensel-phelps-supports-the-fight-against-cancer/index.html">
+                            <div class="news-post-image" style="background-image: url({{asset('wp-content/uploads/2022/10/1705_UCI-Anti-Cancer_OUT_v1_002-1024x684.jpg')}});">
+                                                        	<img width="311" height="225" src="{{asset('wp-content/uploads/2022/10/1705_UCI-Anti-Cancer_OUT_v1_002-311x225.jpg')}}" class="attachment-news-thumbnail size-news-thumbnail wp-post-image" alt="" loading="lazy" />                                                        </div>
+                            <div class="news-post-meta">
+                                <div class="animate-block">
+                                    <div class="category-title">
+									Community                                    </div>
+                                    <div class="post-desc">
+                                        <h2 class="news-post-title">Invested in Women’s Health – Hensel Phel...</h2>
+                                        <div class="post-excerpt">In the US, there are currently over 863,830 women living with cancer. One out of every... <!--more--></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+			        </div>
+        </div>
+		</div>
+		</div><!-- #main -->
+	</div><!-- #primary -->
+
+    @endsection
